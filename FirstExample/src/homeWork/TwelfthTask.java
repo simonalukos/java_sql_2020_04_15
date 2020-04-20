720fd61a371dd60f38f8e78bbef35ebd8abbf2ab
simonalukos <simona.lukoseviciute1@gmail.com>
2020-04-20 17:12:28 +0300

    homeWork Twelfth Task

---
+++ FirstExample/src/homeWork/TwelfthTask.java
@@ -0,0 +1,25 @@
+package homeWork;
+
+import java.util.Arrays;
+
+/**
+ * @author Simona Lukoseviciute
+ */
+public class TwelfthTask {
+
+    public static void main(String[] args) {
+
+
+        String[] vardai = new String[4];
+        vardai[0] = "Aiste";
+        vardai[1] = "Monika";
+        vardai[2] = "Gabriele";
+        vardai[3] = "Veronika";
+        System.out.println(Arrays.toString(vardai));
+    }
+
+    private static void namesFromLetterA(String[] vardai) {
+    }
+
+
+}
