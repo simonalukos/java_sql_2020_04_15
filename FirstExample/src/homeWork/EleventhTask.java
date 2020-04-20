32c3336cb5c287e58296631af72392f88bdfa1e6
simonalukos <simona.lukoseviciute1@gmail.com>
2020-04-20 17:10:36 +0300

    homeWork Eleventh Task

---
+++ FirstExample/src/homeWork/EleventhTask.java
@@ -0,0 +1,20 @@
+package homeWork;
+
+import java.util.Scanner;
+
+/**
+ * @author Simona Lukoseviciute
+ */
+public class EleventhTask {
+    public static void main(String[] args) {
+
+        Scanner scanner =  new Scanner(System.in);
+        String word;
+
+        System.out.println("Irasykite zodi, kuriame yra raidziu 'ab'.");
+        word =  scanner.nextLine();
+
+    }
+    private static void checkWordHasLettersAb(String word, char l1, char l2) {
+    }
+}
