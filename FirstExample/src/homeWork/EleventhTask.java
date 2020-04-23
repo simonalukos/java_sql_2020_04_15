61bc3f8d691778bc0f1e9ace0bbb41492a9685ab
simonalukos <simona.lukoseviciute1@gmail.com>
2020-04-23 21:38:42 +0300

    JDBC tasks

--- FirstExample/src/homeWork/EleventhTask.java
+++ FirstExample/src/homeWork/EleventhTask.java
@@ -11,11 +11,23 @@ public class EleventhTask {
         Scanner scanner =  new Scanner(System.in);
         String word;
 
-        System.out.println("Irasykite zodi, kuriame yra raidziu 'ab'.");
-        
+        System.out.println("Irasykite zodi.");
+        int count = countLetters(scanner.nextLine(), "ab");
+
+
+        System.out.println("Rasta raidziu zodyje " + count);
+
 
     }
-    private static void checkWordHasLettersAb(String word, char l1, char l2) {
-    }
+    private static int countLetters(String word, String  letters){
 
+        int count = 0;
+        for(int i = 0; i < word.length(); i++) {
+            if(letters.charAt(0) == word.charAt(i) && word.length() >= i + 1 && word.charAt(i + 1) == letters.charAt(1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
